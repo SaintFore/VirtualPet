db78067b3419ab1505c1328f22afad54c4d1b350
julien <forevergoodforest@outlook.com>
2025-12-15 17:40:54 +0800

    fix: 修复Feed的测试

---
+++ creature/dog_test.go
@@ -0,0 +1,17 @@
+package creature
+
+import "testing"
+
+func TestFeed(t *testing.T) {
+	p := Pet{Name: "griffin", Hungry: 50}
+	p.Feed()
+	if p.Hungry != 30 {
+		t.Errorf("期望饥饿为30，实际饥饿值为%d", p.Hungry)
+	}
+
+	p.Hungry = 5
+	p.Feed()
+	if p.Hungry != 0 {
+		t.Errorf("期望饥饿为0，实际饥饿值为%d", p.Hungry)
+	}
+}
